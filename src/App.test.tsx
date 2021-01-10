5502ff5aad8d60bf88e557578ca65cf24675b964
ku0513 <kenichi.umeda@gmail.com>
2021-01-11 06:20:00 +0900

    テストコードのリファクタリング

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -9,25 +9,22 @@ const findByTestAttr = (wrapper: ShallowWrapper, val: string) =>
   wrapper.find(`[data-test='${val}']`);
 
 describe("画面表示", () => {
+  const wrapper = shallow(<App />);
   it("レンダリングされていること", () => {
-    const wrapper = shallow(<App />);
     expect(wrapper).toBeTruthy();
   });
 
   it("初回画面表示でlistボタンが表示されていること", () => {
-    const wrapper = shallow(<App />);
     const button = findByTestAttr(wrapper, "list-button");
     expect(button.length).toBe(1);
   });
 
   it("初回画面表示でidが0で表示されていること", () => {
-    const wrapper = shallow(<App />);
     const id = findByTestAttr(wrapper, "id-text");
     expect(id.text()).toContain("0");
   });
 
   it("初回画面表示でnameが空で表示されていること", () => {
-    const wrapper = shallow(<App />);
     const name = findByTestAttr(wrapper, "name-text");
     expect(name.text()).toContain("");
   });
